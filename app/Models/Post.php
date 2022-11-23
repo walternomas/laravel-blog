8864215367ed5302130f2c51f6fd59a77e2b0056
walternomas <w.barriosvazquez@gmail.com>
2022-11-23 18:41:15 -0300

    diseño de la base de datos

---
+++ app/Models/Post.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+  use HasFactory;
+
+  // Relación uno a muchos inversa
+  public function user()
+  {
+    return $this->belongsTo(User::class);
+  }
+
+  public function category()
+  {
+    return $this->belongsTo(Category::class);
+  }
+
+  // Relación muchos a muchos
+  public function tags()
+  {
+    return $this->belongsToMany(Tag::class);
+  }
+
+  // Relación uno a uno polimórfica
+  public function image()
+  {
+    return $this->morphOne(Image::class, 'imageable');
+  }
+}
