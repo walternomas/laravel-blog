9382b1c7479c2f618d9809f543f609f853d0c20a
walternomas <w.barriosvazquez@gmail.com>
2022-11-25 09:42:26 -0300

    me colgué en subir los cambios

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+  public function run()
+  {
+    User::create([
+      'name' => 'Victor Arana Flores',
+      'email' => 'victor.aranaf92@gmail.com',
+      'password' => bcrypt('12345678')
+    ]);
+
+    User::factory(99)->create();
+  }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,20 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use App\Http\Controllers\PostController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [PostController::class, 'index'])->name('posts.index');
+
+Route::get('posts/{post}', [PostController::class, 'show'])->name('posts.show');
+
+Route::get('category/{category}', [PostController::class, 'category'])->name('posts.category');
 
 Route::middleware([
     'auth:sanctum',
